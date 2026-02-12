1e1c4f2d1b53bd554625f711a411b81422fd8d62
Dane <dane@jillybox.com>
2026-02-12 11:06:20 -0800

    fixed treesitter steup

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -46,8 +46,17 @@ return {
         opts = {},
     },
     {
-        'nvim-treesitter/nvim-treesitter',
-        lazy = false,
-        build = ':TSUpdate'
-    },
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function () 
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                -- Ensure specific parsers are installed
+                ensure_installed = { "lua", "go", "svelte", "javascript", "typescript" },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },  
+            })
+        end
+    }
 }
