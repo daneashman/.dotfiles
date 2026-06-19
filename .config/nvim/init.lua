ee5f22fd259c7a1ea55a00a314e680a4aec1c828
Dane Ashman <daneashman@outlook.com>
2026-06-19 14:40:09 -0700

    removed all old vim config files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,49 @@
-require("config.options")
-require("core.lazy")
-require("plugins")
-require("core.lsp")
-require("config.keymaps")
-require("config.autocmds")
-require("config.lualine")
-require("config.misc")
+-- Leader key
+vim.g.mapleader = " "
+-- Keep undos after close
+vim.opt.undofile = true
+-- Window splitting
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+-- Tabs
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+-- Line numbering
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+-- Nerd Font
+vim.g.have_nerd_font = true
+-- Dont show mode because it's in the lualine
+vim.opt.showmode = false
 
+
+vim.keymap.set('n', '<leader>d', vim.cmd.Ex, { desc = 'Open [D]irectory (netrw)' })
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Sync clipboard between OS and Neovim.
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+--
+-- Highlight when yanking (copying) text
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- LSP
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#gopls
+vim.lsp.enable('gopls')
--- .config/nvim/lua/core/lsp.lua
+++
@@ -1,6 +0,0 @@
--- LSP
--- All LSP configs are in ./lsp
--- These were just copied from https://github.com/neovim/nvim-lspconfig/tree/master/lsp
--- They don't change much, so just update whenever you make a new config
-vim.lsp.enable({"lua_ls", "gopls", "svelte"})
-
