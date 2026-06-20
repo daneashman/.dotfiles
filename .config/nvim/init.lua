fdcc3dbf98b8352465e6124696b8a70bdf6ccc94
Dane Ashman <daneashman@outlook.com>
2026-06-20 08:44:38 -0700

    cleaning things up and adding plugins like oil and lsp config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,60 +1,55 @@
--- Leader key
-vim.g.mapleader = " "
--- Keep undos after close
-vim.opt.undofile = true
--- Window splitting
-vim.opt.splitbelow = true
-vim.opt.splitright = true
--- Tabs
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
--- Line numbering
 vim.opt.number = true
 vim.opt.relativenumber = true
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.undofile = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
--- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
--- Nerd Font
+vim.opt.signcolumn = "yes"
+
+vim.g.mapleader = " "
 vim.g.have_nerd_font = true
 
--- Diagnostic config
-vim.diagnostic.config({ virtual_text = true })
+vim.keymap.set("n", "<leader>d", ":Oil<CR>", { desc = "Open [D]irectory (netrw)" })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<leader>p", vim.lsp.buf.format)
 
-vim.keymap.set('n', '<leader>d', vim.cmd.Ex, { desc = 'Open [D]irectory (netrw)' })
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.diagnostic.config({ virtual_text = true })
 
 -- Sync clipboard between OS and Neovim.
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+	vim.opt.clipboard = 'unnamedplus'
 end)
---
+
 -- Highlight when yanking (copying) text
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
--- LSP
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#gopls
-vim.lsp.config['gopls'] = {
-  cmd = { 'gopls' },
-  filetypes = { "go", "gomod", "gowork" },
-  root_markers = { 'go.mod', '.go' },
-  settings = {
-      gopls = {
-          semanticTokens = true
-      }
-  }
-}
-vim.lsp.enable('gopls')
+vim.pack.add({
+	{ src = "https://github.com/rose-pine/neovim" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = 'https://github.com/nvim-mini/mini.icons' },
+	-- To look at:
+	-- mini.pick
+})
+require("mini.icons").setup()
+require "oil".setup({
+	columns = {
+		"icon",
+	},
+})
 
+vim.cmd("colorscheme rose-pine")
+vim.cmd(":hi statusline guibg=NONE")
+
+vim.lsp.enable({ "lua_ls", "gopls" })
