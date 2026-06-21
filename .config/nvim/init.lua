306243ec3ecf77354c2afd2381f51e6dd986c946
Dane Ashman <daneashman@outlook.com>
2026-06-20 23:20:04 -0700

    many updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,22 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.undofile = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes"
+vim.opt.winborder = "rounded"
 
 vim.g.mapleader = " "
 vim.g.have_nerd_font = true
 
-vim.keymap.set("n", "<leader>d", ":Oil<CR>", { desc = "Open [D]irectory (netrw)" })
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<leader>d", ":Oil<CR>", { desc = "Open [D]irectory (netrw)" })
 vim.keymap.set("n", "<leader>p", vim.lsp.buf.format)
 
 vim.diagnostic.config({ virtual_text = true })
@@ -53,3 +54,14 @@ vim.cmd("colorscheme rose-pine")
 vim.cmd(":hi statusline guibg=NONE")
 
 vim.lsp.enable({ "lua_ls", "gopls" })
+
+vim.lsp.config("lua_ls", { -- Stop lsp diagnostic warnings on vim.*
+	settings = {
+		Lua = {
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			}
+		}
+	}
+})
+
