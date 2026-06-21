788f1e09f1f2a6a472e6bcc2860fdaf69e99f313
Dane Ashman <daneashman@outlook.com>
2026-06-21 00:40:05 -0700

    many changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,6 +18,8 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower win
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 vim.keymap.set("n", "<leader>d", ":Oil<CR>", { desc = "Open [D]irectory (netrw)" })
 vim.keymap.set("n", "<leader>p", vim.lsp.buf.format)
+vim.keymap.set({ "n", "v", "x" }, "<leader>s", ":e #<CR>")
+vim.keymap.set({ "n", "v", "x" }, "<leader>S", ":sf #<CR>")
 
 vim.diagnostic.config({ virtual_text = true })
 
@@ -37,9 +39,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 vim.pack.add({
 	{ src = "https://github.com/rose-pine/neovim" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = 'https://github.com/nvim-mini/mini.icons' },
+	{ src = "https://github.com/nvim-mini/mini.icons" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" }
 	-- To look at:
 	-- mini.pick
 })
@@ -49,11 +52,22 @@ require "oil".setup({
 		"icon",
 	},
 })
+require("mason").setup()
+require("nvim-treesitter").setup({
+	ensure_installed = { "go", "svelte", "javascript" },
+	highlight = { enable = true }
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function()
+		pcall(vim.treesitter.start)
+	end,
+})
 
 vim.cmd("colorscheme rose-pine")
 vim.cmd(":hi statusline guibg=NONE")
 
-vim.lsp.enable({ "lua_ls", "gopls" })
+vim.lsp.enable({ "lua_ls", "gopls", "svelte" })
 
 vim.lsp.config("lua_ls", { -- Stop lsp diagnostic warnings on vim.*
 	settings = {
@@ -64,4 +78,3 @@ vim.lsp.config("lua_ls", { -- Stop lsp diagnostic warnings on vim.*
 		}
 	}
 })
-
